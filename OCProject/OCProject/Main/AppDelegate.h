fd234b84c24644bdd34e691b46faeea0844bf09f
Trancephp <trance@TrancedeMacBook-Pro.local>
2017-12-12 17:09:47 +0800

    添加常用第三方库     pod 'MJRefresh'     pod 'MJExtension'     pod 'SDWebImage', '~> 4.0.0'     pod 'AFNetworking', '~> 3.0'     pod 'Masonry', '~> 1.0.2'     pod 'pop', '~> 1.0'     pod 'SVProgressHUD', '~> 2.0.3'     pod 'SDCycleScrollView','~> 1.66'     pod 'MMPopupView'     pod 'IQKeyboardManager'
        # U-Share SDK UI模块（分享面板，建议添加）
        pod 'UMengUShare/UI'
        # 集成新浪微博
        pod 'UMengUShare/Social/Sina'
        # 集成微信
        pod 'UMengUShare/Social/WeChat'
        # 集成QQ
        pod 'UMengUShare/Social/QQ'

---
+++ OCProject/OCProject/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCProject
+//
+//  Created by Trance on 2017/12/12.
+//  Copyright © 2017年 Trance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCProject/OCProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCProject
+//
+//  Created by Trance on 2017/12/12.
+//  Copyright © 2017年 Trance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
